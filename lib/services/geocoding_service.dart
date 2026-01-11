5db8db1c16f6433e6140010a3158e0bcc0725bdd
youssef hussein <20634930+youssefhussein@users.noreply.github.com>
2026-01-11 03:11:16 +0200

    geocoding function + model

--- lib/services/geocoding_service.dart
+++ lib/services/geocoding_service.dart
@@ -1,4 +1,9 @@
+import 'package:emergency_alert/models/geocode.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'package:geocoding/geocoding.dart';
+
+import 'package:http/http.dart';
 
 class GeocodingService {
   Future<String?> reverseGeocode(double lat, double lng) async {
@@ -18,4 +23,27 @@ class GeocodingService {
 
     return parts.isEmpty ? null : parts.join(', ');
   }
+
+
+  Future<String> geocodeAPI(double lat, double lng) async {
+    final apiKey = dotenv.get('GEOCODE_API');
+
+
+    final url = 'https://api.geocode.ai/v1/reverse?key=$apiKey&point.lat=$lat&point.lon=$lng';
+
+    final response = await get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final reverseGeocode = ReverseGeocode.fromRawJson(response.body);
+      if (reverseGeocode.features.isNotEmpty) {
+        final properties = reverseGeocode.features.first.properties;
+        return properties.label;
+      } else {
+        throw Exception('No features found in geocoding response.');
+      }
+    } else {
+      throw Exception('Failed to fetch geocode: ${response.statusCode}');
+    }
+  }
 }
+
+
